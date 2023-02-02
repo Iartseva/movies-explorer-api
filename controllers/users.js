cb27a0a63356b8796718decde5539f1249a0d552
Olga Iartseva <ol-buslaeva@ya.ru>
2023-02-02 21:35:26 +0300

    updatecontrollers

--- controllers/users.js
+++ controllers/users.js
@@ -94,7 +94,7 @@ module.exports.login = (req, res, next) => {
       return res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'lax',
+        sameSite: true,
         /* secure: true, */
       }).send({ token });
     })
