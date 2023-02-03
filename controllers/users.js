42ae409312920170c6e1b507885ccbef7b31e0cb
Olga Iartseva <ol-buslaeva@ya.ru>
2023-02-03 10:27:28 +0300

    changesamesite

--- controllers/users.js
+++ controllers/users.js
@@ -95,8 +95,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: 'None',
-        Secure,
-        /* secure: true, */
+        secure: true,
       }).send({ token });
     })
     .catch(next);
