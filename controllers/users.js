b735f042cc74213105a210b8d85e33ebe072e139
Olga Iartseva <ol-buslaeva@ya.ru>
2023-02-03 12:52:57 +0300

    updateLimiter

--- controllers/users.js
+++ controllers/users.js
@@ -96,7 +96,7 @@ module.exports.login = (req, res, next) => {
         httpOnly: true,
         sameSite: 'None',
         secure: true,
-      }).send(token);
+      }).send({ token });
     })
     .catch(next);
 };
